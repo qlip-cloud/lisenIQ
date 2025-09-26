beb7bad254036b682a867659bd9b9211c41e6f3f
Ashley <ashleydefort@outlook.es>
2025-09-26 18:47:06 +0000

    feat(report): add Survey Response Custom Report All implementation

---
+++ liseniq/liseniq/report/survey_response_custom_report_all/survey_response_custom_report_all.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Mentum Group and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Survey Response Custom Report All"] = {
+	"filters": [
+
+	]
+};

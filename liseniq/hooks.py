b0bbb479581c7b2fa98709904e6f7ec8d68eea46
Ashley <ashleydefort@outlook.es>
2026-02-16 15:42:34 -0500

    fix: update user hooks to link company on user update and improve contact linking logic

--- liseniq/hooks.py
+++ liseniq/hooks.py
@@ -107,7 +107,7 @@ doc_events = {
 		"on_update": "liseniq.utils.api_survey.generate_public_link_for_survey_hook"
 	},
 	"User": {
-		"after_insert": "liseniq.utils.user_hooks.link_company_after_b2c"
+		"on_update": "liseniq.utils.user_hooks.link_company_after_b2c"
 	}
 }
 

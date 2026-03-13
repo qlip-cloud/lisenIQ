0e6f9d38ba84c655296d088c90b339c3cbc26d9d
Adolfo Hernández <161332378+dyald2h93@users.noreply.github.com>
2026-03-13 14:03:56 -0400

    Change default sender name in tasks.py

--- liseniq/tasks.py
+++ liseniq/tasks.py
@@ -9,7 +9,7 @@ from email.utils import formataddr
 from datetime import datetime, timezone
 import pytz
 
-DEFAULT_SENDER_NAME = "OCC Solutions "
+DEFAULT_SENDER_NAME = "Portal de Mediciones"
 
 def _now_utc_str() -> str:
 	return datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
